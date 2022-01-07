fead0aa0bcef03895c6726eda8b87be84af28b15
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-07 10:45:57 -0500

    final touches before publishing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes/notesroutes');
-const htmlRoutes = require('./routes/htmlRoutes/index');
+const apiRoutes = require('./routes/notesroutes');
+const htmlRoutes = require('./routes/index');
 
 
 // Route middleware //
@@ -16,5 +16,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port http://localhost:${PORT}`);
   });
