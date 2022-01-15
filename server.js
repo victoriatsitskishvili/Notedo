e622f9a352ede85b13eaa1a92a4c2aa049ec807d
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-15 13:58:57 -0500

    updated entire project and found a different solution

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
-const express = require('express');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var { v4: uuidv4 } = require("uuid");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/notesroutes');
-const htmlRoutes = require('./routes/index');
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3001;
 
-
-// Route middleware //
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.json());app.use(express.static("public"));
 
-// Use apiRoutes //
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-app.listen(PORT, () => {
-    console.log(`API server now on port http://localhost:${PORT}`);
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", 'utf8', function (err, data) {
+    res.json(JSON.parse(data));
   });
+});
+
+app.post("/api/notes", function (req, res) {
+  var title = req.body.title;
+  var text = req.body.text;
+  var newNote = { title, text, id: uuidv4() };
+  fs.readFile("./db/db.json", function (err, data) {
+    if(err) throw err;
+    var currentNotes = JSON.parse(data); 
+    currentNotes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(currentNotes), function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Saved a new note");
+      }
+    })
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  })
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  var clicked = req.params.id;
+  console.log('clicked: ' + clicked);
+  fs.readFile('./db/db.json', function (err, data) {
+    var json = JSON.parse(data)
+    var filtered = json.filter(note => note.id !== clicked)
+    fs.writeFile("./db/db.json", JSON.stringify(filtered), function (err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("Note Deleted");
+      }
+    })
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  })
+});
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
